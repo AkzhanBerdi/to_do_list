a2692f2759ffcd0adba3f4da7157ef59710d02f0
AkzhanBerdi <akzhan.berdeyev@gmail.com>
2023-05-09 01:00:26 +0600

    choices created

--- display/views.py
+++ display/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from .models import Task
+from django.core.exceptions import ValidationError
+from django import forms
+
+# class InvalidDateFormatError(ValidationError):
+#     pass
+
+# class TaskForm(forms.ModelForm):
+#     class Meta:
+#         model = Task
+#         fields = ['objective', 'status', 'dead_line']
 
 # Create your views here.
 def index_view(request):
@@ -14,13 +24,13 @@ def new_task(request):
     if request.method == 'GET':
         return render(request,'new_task.html',{'choices': choices})
     elif request.method == 'POST':
+        task = Task.objects.all()
         objective = request.POST.get('objective')
         status = 'new'
         date = request.POST.get('date')
-        Task.objects.create(objective = objective, status = status, dead_line= date)
+        Task.objects.create(objective = objective, status = status, dead_line = date)
 
-        return render(request, 'list_task.html')
-
+        return render(request, 'list_task.html', context={'task': task})
 
 def list_task(request):
     task = Task.objects.all()
