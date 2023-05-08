f89bcf3f2884afeb6e8161b8daf26f12112f2e9e
AkzhanBerdi <akzhan.berdeyev@gmail.com>
2023-05-08 15:44:12 +0600

    DB included fields and _str_ method

--- display/models.py
+++ display/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tesks(models.Model):
+    objective = models.CharField(max_length=25, null=False, blank=False)
+    status = models.CharField(max_length=10, null=False, default='New')
+    dead_line = models.DateField(auto_now=False, auto_now_add=False, blank=True, default=None)
+
+    def __str__(self):
+        return f"{self.pk}.{self.objective} - {self.status} || {self.dead_line}"
