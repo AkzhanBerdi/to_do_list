2ca5f8e136e5f18d151ed0da03fc677fe3a95987
AkzhanBerdi <akzhan.berdeyev@gmail.com>
2023-05-08 15:12:14 +0600

    display app, views and templates created

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# import sys
+# sys.path.append("..")
 from django.contrib import admin
 from django.urls import path
+from display import views as display_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', display_views.index_view),
 ]
